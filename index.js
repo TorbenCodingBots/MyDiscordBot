0fb3d922bc94248bc8b99e63b07ee3bd3c1eda07
TorbenCodingBots <torben.bots@gmail.com>
2021-09-22 13:45:08 -0400

    2 files modified

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+const Client = new.Discord.Client();
+
+client.once('ready', () => {
+  console.log('bot is online');
+});
+
+client.login('ODkwMjY2MTM3MzE5ODY2NDA5.YUtS8w.yDyAi9YbQM84tUkiGw82aTSU19Q ');
